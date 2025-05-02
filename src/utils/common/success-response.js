2eb3358687c18c9c544e99dca5d94c761dacd67a
NIRAJKrYADAV145 <niraj20802@gmail.com>
2025-05-02 17:35:34 +0530

    basic flight services with simple CRUD operation

---
+++ src/utils/common/success-response.js
@@ -0,0 +1,8 @@
+//  making custom error object.
+const success = {
+    success: true,
+    message: ' Successfully completed the request',
+    data: {},
+    error: {}
+}
+module.exports = success;
